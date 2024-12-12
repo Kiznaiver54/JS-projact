b8a41fabd8fbe08f1068e03013fdbe926ac73280
Алексей <166537499+Kiznaiver54@users.noreply.github.com>
2024-12-12 17:53:21 +0300

    Add files via upload

---
+++ script 5.10.1.js
@@ -0,0 +1,8 @@
+let userNumber = prompt("Пожалуйста, введите любое число:");
+
+let number = Number(userNumber);
+
+let square = number * number;
+let cube = number * number * number;
+
+alert("Вы ввели число - " + number + "\nКвадрат - " + square + "\nКуб - " + cube);
